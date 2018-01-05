19253713571f4688823b98cab2b8574e8ffed5b7
Jacky <email.com>
2018-01-05 18:20:26 +1100

    DemoSquares
    Area of 10 squares with length n+1

---
+++ Chapter9/Chapter9/DemoSquares.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter9
+{
+    class DemoSquares
+    {
+        public static void Run(string[] args)
+        {
+            //Instantiate an array with 10 squares with n+1 side lengths
+            Square[] squares = new Square[10];
+            for(int x=0;x<squares.Length;x++)
+            {
+                squares[x] = new Square(x + 1);
+                //Display areas of each square
+                Console.WriteLine("Area: {0}",squares[x].Area);
+            }
+        }
+    }
+}
---
+++ Chapter9/Chapter9/Square.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter9
+{
+    class Square
+    {
+        public int Side { get; set; }
+        public int Area { get; set; }
+
+        public Square(int sideLength)
+        {
+            this.Side = sideLength;
+            ComputerSquareArea();
+        }
+        private void ComputerSquareArea()
+        {
+            this.Area = this.Side * this.Side;
+        }
+    }
+}

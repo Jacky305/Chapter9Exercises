abe0f9bfa00082b26c9185fe71112ed942d48303
Jacky <email.com>
2018-01-05 17:17:06 +1100

    Initial Commit

---
+++ Chapter9/Chapter9/Pizza.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter9
+{
+    class Pizza
+    {
+        public string Toppings { get; set; }
+        public int Diameter { get; set; }
+        public double price { get; set; }
+
+
+    }
+}
---
+++ Chapter9/Chapter9/TestPizza.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter9
+{
+    class TestPizza
+    {
+        public static void Run(string[] args)
+        {
+            //Instantiate 1 Pizza object
+            Pizza pizza = new Pizza();
+            //Demonstrate use of set and get accessors
+            pizza.Toppings = "Ham";
+            Console.WriteLine("Pizza's topping: {0}", pizza.Toppings);
+
+            pizza.Toppings = "Cheese";
+            Console.WriteLine("Pizza's topping now: {0}", pizza.Toppings);
+        }
+    }
+}

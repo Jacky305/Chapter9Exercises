3afbbd967b21d3dd4efec3a3f48d9e6c94fc01f2
Jacky <email.com>
2018-01-05 21:17:21 +1100

    FriendList
    Sorts friends by first name.

---
+++ Chapter9/Chapter9/Friend.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter9
+{
+    class Friend : IComparable
+    {
+        private string name;
+        private string phoneNumber;
+        private int birthday;
+
+        public string Name { get { return name; } set { name = value; } }
+        public string PhoneNumber { get { return phoneNumber; } set { phoneNumber = value; } }
+        public int Birthday { get { return birthday; } set { birthday = value; } }
+
+        int IComparable.CompareTo(object obj)
+        {
+            int x;
+            Friend temp = (Friend)obj;
+
+            x = this.name.CompareTo(temp.name);
+            return x;
+        }
+    }
+}
---
+++ Chapter9/Chapter9/FriendsList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter9
+{
+    class FriendsList
+    {
+        public static void Run(string[] args)
+        {
+            Friend[] friendsList = new Friend[4];
+
+            for(int x=0; x<friendsList.Length; x++)
+            {
+                Console.WriteLine("------------------------------------------");
+                friendsList[x] = new Friend();
+                Console.WriteLine("Enter name for Friend {0}: ",x);
+                friendsList[x].Name = Console.ReadLine();
+                Console.WriteLine("Enter phone number for Friend {0}: ", x);
+                friendsList[x].PhoneNumber = Console.ReadLine();
+                Console.WriteLine("Enter birthday for Friend {0}: ", x);
+                friendsList[x].Birthday = int.Parse(Console.ReadLine());
+            }
+
+            Array.Sort(friendsList);
+            Console.WriteLine("\n------------------------------------------");
+            Console.WriteLine("Sorted by first name:");
+            foreach(Friend friend in friendsList)
+            {
+                Console.WriteLine("{0}", friend.Name);
+            }
+        }
+    }
+}
